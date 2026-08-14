163d73c4a4d7dad57cf4f2baf73b6f4ed21fa8b4
Sasha76 <sashahuer@gmail.com>
2026-08-14 20:35:55 +0200

    Delete axioOS/Win11_CMD_System/Applications/Clock.ext.bat

--- axioOS/Win11_CMD_System/Applications/Clock.ext.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Clock App - Win11 CMD
-:loop
-cls
-echo =====================================
-echo  HORLOGE SYSTEME : %time:~0,8%
-echo =====================================
-timeout /t 1 >nul
-goto loop
